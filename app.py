2a5a83f58e91aa2777e5964b20549e5298369b9c
momnpa333 <momnpa333@gmail.com>
2022-05-30 21:50:42 +0900

    url 받아오기

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # !usr/bin/python3
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
+    # get 을 통한 전달받은 테이터를 확인
+    key1 = request.args.get("urls")
+    print(key1)
+
     return render_template("home.html")
 
 @app.route('/home.html')
@@ -21,5 +25,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
 
