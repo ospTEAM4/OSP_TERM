aa96a6da7f77fd3af5bf3b158458778022de2e11
HYOJUNG08 <hjjung933@gmail.com>
2022-05-30 17:38:16 +0900

    add html, css

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template("hello.html")
+def home():
+    return render_template("home.html")
 
+@app.route('/home.html')
+def home2():
+    return render_template("home.html")
 
-if __name__ == '__main__':
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
+if __name__=="__main__":
     app.run()
 
