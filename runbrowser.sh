0160c9665651f13cebb0a13943db607decefb33b
momnpa333 <momnpa333@gmail.com>
2022-06-07 01:28:18 +0900

    make shell file

---
+++ runbrowser.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+firefox http://127.0.0.1:5000/
+
+echo "success"
---
+++ runserver.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+read -p "apt-get update를 위한 사용자 패스워드를 입력해주세요 : " PW
+
+echo "$PW" | sudo -kS apt-get update
+echo "$PW" | sudo -kS apt install software-properties-common
+echo "$PW" | sudo -kS apt install python3.9
+python3 --version
+echo "$PW" | sudo pip install -r requirements.txt
+
+chmod 700 runbrower.sh
+
+gnome-terminal -e ./runbrowser.sh
+
+python3 app.py
