353de96114d5354825ee7131d0c4ca07462f1db4
momnpa333 <momnpa333@gmail.com>
2022-06-09 03:46:58 +0900

    fix runbrowser.sh

--- runbrowser.sh
+++ runbrowser.sh
@@ -2,7 +2,6 @@
 
 python3 app.py
 
-
 firefox http://127.0.0.1:5000/
 
 echo "success"
--- runserver.sh
+++ runserver.sh
@@ -5,8 +5,14 @@ read -p "apt-get update를 위한 사용자 패스워드를 입력해주세요 :
 echo "$PW" | sudo -kS apt-get update
 echo "$PW" | sudo -kS apt install software-properties-common
 echo "$PW" | sudo -kS apt install python3.9
+echo "$PW" | sudo apt install default-jdk
+
 python3 --version
 echo "$PW" | sudo pip install -r requirements.txt
+
+chmod 700 geckodriver
+
+echo "$PW" | mv geckodriver /usr/local/bin/
 
 chmod 700 runbrowser.sh
 
