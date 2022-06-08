bec1748085d391f9d07813a4707ed491289a35b7
HYOJUNG08 <hjjung933@gmail.com>
2022-06-08 21:09:50 +0900

    passing data to home.html

--- app.py
+++ app.py
@@ -19,8 +19,20 @@ def home():
         comment=google_keyword.result(key1)
         keyword,preference,preferenceRates=cut_word.cut_word(comment)
         database.push_data(key1, preferenceRates)
+        element=database.find_data(key1)
+        dates=[]
+        likes=[]
+        dislikes=[]
+        neu=[]
+        for i in range(0,len(element['record'])):
+            x=list(element['record'][i]['time_record'].values())
+            tmp=' '.join(s for s in x)
+            dates.append(tmp)
+            likes.append(element['record'][i]['result_record']['like'])
+            dislikes.append(element['record'][i]['result_record']['dislike'])
+            neu.append(element['record'][i]['result_record']['neutral'])
         print(111)
-    return render_template("home.html",keyword=keyword,preference=preference)
+    return render_template("home.html",keyword=keyword,preference=preference,element=element,dates=dates,likes=likes,dislikes=dislikes,neu=neu)
    
 
 
