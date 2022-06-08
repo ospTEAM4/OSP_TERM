7d2f4c64bb0b83688e844bea6f3e64af5887aca7
momnpa333 <momnpa333@gmail.com>
2022-06-08 19:51:44 +0900

    fix ruserver.sh

--- runserver.sh
+++ runserver.sh
@@ -2,13 +2,19 @@
 
 read -p "apt-get update를 위한 사용자 패스워드를 입력해주세요 : " PW
 
+
+#크롬 버전 확인
+
 echo "$PW" | sudo -kS apt-get update
 echo "$PW" | sudo -kS apt install software-properties-common
 echo "$PW" | sudo -kS apt install python3.9
 python3 --version
 echo "$PW" | sudo pip install -r requirements.txt
 
+chmod 700 geckodriver
 
-chmod 700 runbrower.sh
+echo "$PW" | sudo mv geckodriver /usr/local/bin/
+
+chmod 700 runbrowser.sh
 
 gnome-terminal -e ./runbrowser.sh
